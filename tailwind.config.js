e14564887cae8aef15be675138263f461fabccba
Nic Goodman <nicgoodman@Captain-Ramius.lan>
2020-07-24 10:50:56 -0400

    first file commit for textura

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,31 @@
 module.exports = {
-  purge: [],
-  theme: {
-    extend: {},
+  purge: {
+    enabled: false,
+    content: ['./templates/**/*.html',
+      './templates/**/*.twig',
+    ],
   },
-  variants: {},
+  theme: {
+    extend: {
+      colors: {
+        'rug-black': '#11111e',
+        'rug-dark-blue': '#353a49',
+        'rug-yellow': '#feba40',
+        'rug-grey': '#c6c6c6',
+        'rug-light-blue': '#bde3ef',
+        'rug-white': '#fffdfd',
+      },
+    },
+    fontFamily: {
+      'nexa-rust-sans': ['', 'sans-serif'],
+      'bioRhyme': ['', 'serif'],
+      'quicksand': ['', 'sans-serif'],
+    }
+  },
+  variants: {
+    zIndex: ['responsive', 'hover', 'focus'],
+    textColor: ['responsive', 'hover', 'focus', 'active', 'group-hover'],
+  },
   plugins: [
     require('@tailwindcss/ui'),
   ],
