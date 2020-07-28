3f691f9aaecea39e43822c4a58cde826b6a90208
Nic Goodman <nicgoodman@Captain-Ramius.lan>
2020-07-28 13:24:02 -0400

    added in new min height variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,14 @@ module.exports = {
         'rug-light-blue': '#bde3ef',
         'rug-white': '#fffdfd',
       },
+      minHeight: {
+        '0': '0',
+        '1/4': '25%',
+        '1/2': '50%',
+        '3/4': '75%',
+        'full': '100%',
+        '2/3-screen': '66vh',
+      },
     },
     fontFamily: {
       'nexa-rust-sans': ['NexaRustSans-Book', 'sans-serif'],
