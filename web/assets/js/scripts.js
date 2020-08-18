b57322225a1a4df69544c7a3c1a2304fd36bb90a
Nic Goodman <nicgoodman@Captain-Ramius.lan>
2020-08-18 14:36:22 -0400

    changed mybutton

--- web/assets/js/scripts.js
+++ web/assets/js/scripts.js
@@ -113,9 +113,9 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
+    contactButton.style.display = "block";
   } else {
-    mybutton.style.display = "none";
+    contactButton.style.display = "none";
   }
 }
 
