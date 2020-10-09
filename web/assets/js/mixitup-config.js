816069987c56f53eeac93f921f3d9165b93e26f1
Nic Goodman <nicgoodman@Captain-Ramius.lan>
2020-10-09 17:47:00 -0400

    testing observer for rugblocks

--- web/assets/js/mixitup-config.js
+++ web/assets/js/mixitup-config.js
@@ -31,4 +31,34 @@ $(function () {
         mixer.sort('sort', 'size:' + sortOrder);
     });
 });
+function lazyload() {
+    //lazy load using IntersectionObserver API
+    var images = document.querySelectorAll('[data-src]'); //look at images with data-src
+    var config = {
+      rootMargin: '0px 0px 50px 0px',
+      threshold: 0
+    };
+    var loaded = 0;
+
+    var observer = new IntersectionObserver(function (entries, self) {
+      entries.forEach(function (entry) {
+        if (entry.isIntersecting) {
+            //console.log("Image" .concat(entry.target.src, "is in the viewport!"));
+          preloadImage(entry.target);
+          // Stop watching and load the image
+          self.unobserve(entry.target);
+        }
+      });
+    }, config);
+
+    images.forEach(function(image) {
+      observer.observe(image);
+    });
+    //set each observed image src to its data-src value
+    function preloadImage(img) {
+      var src = img.getAttribute('data-src');
+      if (!src) { return; }
+      img.src = src;
+    }
+}
   });
