b4feadf0ada347136e2a41b4038cc7e5f0ca60e2
Nic Goodman <nicgoodman@Captain-Ramius.lan>
2020-10-09 17:08:34 -0400

    separated mixitup and added favicons

---
+++ web/assets/js/mixitup-config.js
@@ -0,0 +1,32 @@
+var chevronSize = document.getElementById("chevron-size");
+$(function () {
+    var sortOrder = 'asc',
+        $toggleSortSize = $('.toggle-sort-size');
+
+    var mixer = mixitup('#feed', {
+        multifilter: {
+            enable: true // enable the multifilter extension for the mixer
+        },
+        pagination: {
+            limit: 24
+        },
+        load: {
+            sort: 'size:asc'
+        }
+    });
+    $toggleSortSize.on('click', function () {
+        $('button').removeClass('text-rug-grey');
+        $(this).addClass('text-rug-grey');
+        switch (sortOrder) {
+            case 'asc':
+                sortOrder = 'desc';
+                chevronSize.classList.add("rotate-180");
+                break;
+            case 'desc':
+                sortOrder = 'asc';
+                chevronSize.classList.remove("rotate-180");
+                break;
+        }
+        mixer.sort('sort', 'size:' + sortOrder);
+    });
+});
